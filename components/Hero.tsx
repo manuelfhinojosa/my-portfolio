1707f46554bb64ad36aaf8da1ff2c7e4dd62e3ad
manuelfhinojosa <ingmanuelhinojosa@gmail.com>
2026-02-07 00:08:29 -0700

    Added image to hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,28 @@
+import Image from "next/image";
 export default function Hero() {
   return (
-    <section id="hero" className="h-screen flex items-center">
-      <div className="max-w-7xl mx-auto px-6">
-        <h1 className="text-5xl font-bold">Manuel Hinojosa</h1>
-        <p className="mt-6 text-xl text-gray-600 max-w-xl">
-          {" "}
-          I&apos;m a Software Engineer, focused on building secure and scalable
-          systems.
-        </p>
-        <p className="mt-6 text-xl text-gray-600 max-w-xl">
-          Python • TypeScript • Backend • Full Stack • AI-ready
-        </p>
+    <section id="hero" className="h-screen flex justify-center">
+      <div className="flex flex-col md:flex-row gap-12 items-center">
+        <div className="max-w-7xl mx-auto px-6">
+          <h1 className="text-5xl font-bold">Manuel Hinojosa</h1>
+          <p className="mt-6 text-xl text-gray-600 max-w-xl">
+            {" "}
+            Software Engineer — Committed to building secure and scalable
+            systems.
+          </p>
+          <p className="mt-6 text-xl text-gray-600 max-w-xl">
+            Python • TypeScript • Backend • Full Stack • AI-ready
+          </p>
+        </div>
+        <div className="md:w-1/2 flex">
+          <Image
+            src="/images/PXL_20250521_015333124.MP.jpg"
+            width={192}
+            height={192}
+            alt="Hero Image"
+            className="rounded-xl object-cover"
+          />
+        </div>
       </div>
     </section>
   );
